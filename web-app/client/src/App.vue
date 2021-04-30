577fc7758e33e086ca0336eea7f03f670de8092f
Enrique Abrahm García Torres <garcia.enrique.torres@gmail.com>
2021-04-30 14:11:07 -0500

    Modificar estilo de la pagina

--- web-app/client/src/App.vue
+++ web-app/client/src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-        <p><router-link to="/">Home</router-link>&nbsp;
-        <!-- <router-link to="/castBallot">Cast Ballot</router-link>&nbsp; -->
+    <header>
+
+    </header>
+        <!-- <p><router-link to="/">Home</router-link>&nbsp; -->
+        <!-- <router-link to="/castBallot">Cast Ballot</router-link>&nbsp;
         <router-link to="/QueryAll">QueryAll</router-link></p>
         <router-link to="/queryWithQueryString">Query by Type</router-link>&nbsp;
         <router-link to="/queryByKey">Query by Key</router-link>&nbsp;
-        <router-link to="/getCurrentStanding">Get Poll Standings</router-link>&nbsp;
+        <router-link to="/getCurrentStanding">Get Poll Standings</router-link>&nbsp; -->
       
       <router-view></router-view>
     </div>
 </template>
+
+<style scoped>
+  header {
+    background-image: url("./assets/background.jpeg");
+    background-size: contain;
+    background-position: center top;
+    height: 250px;
+  }
+</style>
 
 <script>
 
@@ -19,13 +31,16 @@ export default {
 </script>
 
 <style>
+body {
+  padding: 0px;
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 .chart-wrapper {
   width: 100%;
--- web-app/client/src/components/Home.vue
+++ web-app/client/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="posts">
-    <h1>2020 Presidential Election</h1>
-    <h3>If you are a registered voter, enter your voterId below</h3>
+    <h1>Elecciones IBM 2021</h1>
+    <h3>Ingrese la clave de elevtor</h3>
     <!--span><b>{{ response }}</b></span><br /-->
     <form v-on:submit="validateVoter">
       <input type="text" v-model="loginData.voterId" placeholder="Enter VoterId">
@@ -17,9 +17,153 @@
     </form>
 
     <br>
+
+    <div class="INE">
+      <img class="INE-photo" src="../assets/person.png" alt="INE photo">
+      <img class="INE-photo2" src="../assets/person.png" alt="INE photo2">
+      <div class="INE-sex">
+        <p>SEXO: M</p>
+      </div>
+      <div class="INE-name">
+        <p><b>NOMBRE</b></p>
+        <p>GOMEZ</p>
+        <p>VELAZQUEZ</p>
+        <p>MARGARITA</p>
+      </div>
+      <div class="INE-address">
+        <p><b>DOMICILIO</b></p>
+        <p>VIADUCTO TLALPAN NO 100</p>
+        <p>COL. ARENAL TEPEPAN, ALCADIA</p>
+        <p>TLALPAN, C.P. 14610, CIUDAD DE MEXICO</p>
+      </div>
+      <div class="INE-codes">
+        <p><b style="margin-right: 5px;">CLAVE DE ELECTOR</b> GMVLMR80070501M100</p>
+        <p><b>CURP</b></p>
+        <p>GOVM800705MCLMR01</p>
+      </div>
+      <div class="INE-extra">
+        <div class="INE-extra-dob">
+          <p><b>FECHA DE NACIMIENTO</b></p>
+          <p>20/20/2020</p>
+        </div>
+        <div class="INE-extra-section">
+          <p><b>SECCIÓN</b></p>
+          <p>20/20/2020</p>
+        </div>
+        <div class="INE-extra-validity">
+          <p><b>VIGENCIA</b></p>
+          <p>2016-2020</p>
+        </div>
+      </div>
+    </div>
     <vue-instant-loading-spinner id='loader' ref="Spinner"></vue-instant-loading-spinner>
   </div>
 </template>
+
+<style scoped>
+  .INE {
+    margin: 0 auto;
+    width: 920px;
+    height: 536px;
+    background-image: url("../assets/INE.png");
+    background-size: contain;
+    position: relative;
+  }
+  .INE .INE-photo {
+    height: 230px;
+    position: absolute;
+    left: 32px;
+    bottom: 100px;
+  }
+  .INE .INE-photo2 {
+    height: 130px;
+    position: absolute;
+    right: 65px;
+    top: 235px;
+  }
+
+  .INE .INE-sex {
+    position: absolute;
+    top: 175px;
+    right: 60px;
+  }
+
+  .INE .INE-name p {
+    display: flex;
+    width: 350px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 17px;
+  }
+
+  .INE .INE-name {
+    position: absolute;
+    top: 175px;
+    left: 200px;
+  }
+
+  .INE .INE-name p {
+    display: flex;
+    width: 350px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 17px;
+  }
+
+  .INE .INE-address {
+    position: absolute;
+    bottom: 175px;
+    left: 200px;
+  }
+
+  .INE .INE-address p {
+    display: flex;
+    width: 350px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 17px;
+  }
+
+  .INE .INE-codes {
+    position: absolute;
+    bottom: 100px;
+    left: 200px;
+  }
+
+  .INE .INE-codes p {
+    display: flex;
+    width: 550px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 17px;
+  }
+
+  .INE .INE-extra {
+    position: absolute;
+    bottom: 45px;
+    left: 200px;
+    display: flex;
+  }
+
+  .INE .INE-extra div.INE-extra-dob {
+    width: 230px;
+  }
+
+  .INE .INE-extra div.INE-extra-section {
+    width: 230px;
+  }
+
+  .INE .INE-extra div.INE-extra-validity {
+    width: 230px;
+  }
+
+  .INE .INE-extra div p {
+    text-align: left;
+    margin: 0px;
+    padding: 0px;
+    font-size: 17px;
+  }
+</style>
 
 <script>
 import PostsService from "@/services/apiService";
